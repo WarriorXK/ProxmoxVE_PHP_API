4eba28fd1c669e661a2adca45e8221f047c52b6f
Kevin Meijer <kevinmeijer@brightfish.nl>
2021-10-05 10:14:15 +0200

    Added support for authentication set/get

--- src/Request.php
+++ src/Request.php
@@ -21,15 +21,28 @@ class Request {
     protected static $ticket;
 
     /**
+     * @var string|null
+     */
+    protected static $_CSRFPreventionToken = NULL;
+
+    /**
+     * @var string|null
+     */
+    protected static $_PVEAuthCookie = NULL;
+
+    /**
      * @var \Curl\Curl
      */
     protected static $Client;
 
     /**
-     * Proxmox Api client
-     * @param array $configure   hostname, username, password, realm, port
-    */
-    public static function Login(array $configure, $verifySSL = FALSE, $verifyHost = FALSE) {
+     * @param array $configure hostname, username, password, realm, port
+     * @param bool  $verifySSL
+     * @param bool  $verifyHost
+     *
+     * @throws \Proxmox\ProxmoxException
+     */
+    public static function Login(array $configure, bool $verifySSL = FALSE, bool $verifyHost = FALSE) {
         $check = FALSE;
         self::$hostname = !empty($configure['hostname'])  ? $configure['hostname']  : $check = TRUE;
         self::$username = !empty($configure['username'])  ? $configure['username']  : $check = TRUE;
@@ -39,19 +52,75 @@ class Request {
         if ($check) {
             throw new ProxmoxException('Require in array [hostname], [username], [password], [realm], [port]');
         }
-        self::ticket($verifySSL, $verifyHost);
+        self::_Ticket($verifySSL, $verifyHost);
     }
+
+    protected static function _GetClient() : \Curl\Curl {
+
+        if (self::$Client === NULL) {
+            self::$Client = new \Curl\Curl();
+        }
+
+        return self::$Client;
+    }
+
+    public static function TestAuthentication(string $preventionToken, string $authCookie, string $hostname, int $port, bool $verifySSL = FALSE, bool $verifyHost = FALSE) : bool {
+
+        $client = new \Curl\Curl();
+        $client->setHeader('CSRFPreventionToken', $preventionToken);
+        $client->setCookie('PVEAuthCookie', $authCookie);
+        $client->setOpts([
+            CURLOPT_SSL_VERIFYPEER => $verifySSL,
+            CURLOPT_SSL_VERIFYHOST => $verifyHost
+        ]);
+
+        $resp = $client->get('https://' . $hostname . ':' . $port . '/api2/json/cluster');
+
+        return $client->httpStatusCode === 200;
+    }
+
+    /**
+     * @param string $preventionToken
+     * @param string $authCookie
+     * @param string $hostname
+     * @param int    $port
+     */
+    public static function SetAuthentication(string $preventionToken, string $authCookie, string $hostname, int $port) : void {
+
+        static::$_CSRFPreventionToken = $preventionToken;
+        static::$_PVEAuthCookie = $authCookie;
+        static::$hostname = $hostname;
+        static::$port = $port;
+
+        $client = static::_GetClient();
+
+        $client->setHeader('CSRFPreventionToken', static::$_CSRFPreventionToken);
+        $client->setCookie('PVEAuthCookie', static::$_PVEAuthCookie);
+
+    }
+
+    /**
+     * @return string[]
+     */
+    public static function GetAuthentication() : array {
+        return [
+            'CSRFPreventionToken' => static::$_CSRFPreventionToken,
+            'PVEAuthCookie'       => static::$_PVEAuthCookie,
+        ];
+    }
+
     /**
      * Create or verify authentication ticket.
      * POST /api2/json/access/ticket
     */
-    protected static function ticket($verifySSL, $verifyHost) {
-        self::$Client = new \Curl\Curl();
-        self::$Client->setOpts([
+    protected static function _Ticket($verifySSL, $verifyHost) {
+
+        $client = static::_GetClient();
+        $client->setOpts([
             CURLOPT_SSL_VERIFYPEER => $verifySSL,
             CURLOPT_SSL_VERIFYHOST => $verifyHost
         ]);
-        $response = self::$Client->post('https://' . self::$hostname . ':' . self::$port . '/api2/json/access/ticket', [
+        $response = $client->post('https://' . self::$hostname . ':' . self::$port . '/api2/json/access/ticket', [
             'username'  => self::$username,
             'password'  => self::$password,
             'realm'     => self::$realm,
@@ -59,16 +128,15 @@ class Request {
 
         if (!isset($response->data)) {
 
-            if (self::$Client->errorCode > 0) {
-                throw new ProxmoxException('CURL error: ' . self::$Client->errorMessage . ' (Code ' . self::$Client->errorCode . ')');
+            if ($client->errorCode > 0) {
+                throw new ProxmoxException('CURL error: ' . $client->errorMessage . ' (Code ' . $client->errorCode . ')');
             }
 
             throw new ProxmoxException('Response data empty');
         }
-        // set header
-        self::$Client->setHeader('CSRFPreventionToken', $response->data->CSRFPreventionToken);
-        // set cookie
-        self::$Client->setCookie('PVEAuthCookie', $response->data->ticket);
+
+        static::SetAuthentication($response->data->CSRFPreventionToken, $response->data->ticket, self::$hostname, self::$port);
+
         return TRUE;
     }
     /**
